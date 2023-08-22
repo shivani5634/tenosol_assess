dedaf212bf6210fb1dce912c7fb6a5dee83a6d0b
Shivani Gupta <73272177+shivani5634@users.noreply.github.com>
2023-08-22 23:30:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="action.php">
+    <h1>Query Form</h1><br>
+    <label for="fullName">FullName</label>
+    <input type="text" name="fullName" id="fullName"><br>
+    <label for="number">Number</label>
+    <input type="number" name="number" id="number"><br>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email"><br>
+    <label for="subject">Subject</label>
+    <input type="text" name="subject" id="subject"><br>
+    <label for="message">Message</label>
+    <textarea type="text" name="message" id="message"></textarea><br>
+    <input type="submit" name="submitbtn">
+    </form>
+
+</body>
+</html>
